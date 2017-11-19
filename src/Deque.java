27b62b33bf27fe8917b0468b112fa89898ce426f
Deng <d.vay@qq.com>
2017-11-19 11:48:06 +0800

    Fix the bug of Deque.java

--- src/Deque.java
+++ src/Deque.java
@@ -1,31 +1,32 @@
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
-public class Deque<T> implements Iterable<T> {
+public class Deque<Item> implements Iterable<Item> {
 
-    private Node first = null;
-    private Node last = null;
-    private int n = 0;
+    private Node first;
+    private Node last;
+    private int n;
 
     private class Node {
-        T item;
+        Item item;
         Node next;
         Node prev;
     }
 
     public Deque() {                           // construct an empty deque
         first = last = null;
+        n = 0;
     }
 
     public boolean isEmpty() {                 // is the deque empty?
-        return first == null;
+        return n == 0;
     }
 
     public int size() {                        // return the number of items on the deque
         return n;
     }
 
-    public void addFirst(T item) {          // add the item to the front
+    public void addFirst(Item item) {          // add the item to the front
         if (item == null) {
             throw new IllegalArgumentException();
         }
@@ -33,11 +34,17 @@ public class Deque<T> implements Iterable<T> {
         node.item = item;
         node.next = first;
         node.prev = null;
+
+        if (isEmpty()) { // Be careful!
+            last = node;
+        } else {
+            first.prev = node;
+        }
         first = node;
         ++n;
     }
 
-    public void addLast(T item) {          // add the item to the end
+    public void addLast(Item item) {          // add the item to the end
         if (item == null) {
             throw new IllegalArgumentException();
         }
@@ -45,53 +52,69 @@ public class Deque<T> implements Iterable<T> {
         node.item = item;
         node.prev = last;
         node.next = null;
-        last.next = node;
 
+        if (isEmpty()) {
+            first = node;
+        } else {
+            last.next = node;
+        }
         last = node;
         ++n;
     }
 
-    public T removeFirst() {               // remove and return the item from the front
+    public Item removeFirst() {               // remove and return the item from the front
         if (isEmpty()) {
             throw new NoSuchElementException();
         }
-        T item = first.item;
+        Item item = first.item;
         first = first.next;
         --n;
         if (isEmpty()) {
             last = null;
+        } else {
+            first.prev = null;
         }
         return item;
     }
 
-    public T removeLast() {                // remove and return the item from the end
+    public Item removeLast() {                // remove and return the item from the end
         if (size() == 0) {
             throw new NoSuchElementException();
         }
-        T item = last.item;
+        Item item = last.item;
         last = last.prev;
         --n;
-        if (n == 0) {
+        if (isEmpty()) {
             first = null;
+        } else {
+            last.next = null;
         }
         return item;
     }
 
     @Override
-    public Iterator<T> iterator() {         // return an iterator over items in order from front to end
-        return new Iterator<T>() {
+    public Iterator<Item> iterator() {         // return an iterator over items in order from front to end
+        return new Iterator<Item>() {
             Node cur = first;
 
             @Override
             public boolean hasNext() {
-                return cur.next != null;
+                return cur != null;
             }
 
             @Override
-            public T next() {
-                T item = cur.item;
+            public Item next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                Item item = cur.item;
                 cur = cur.next;
                 return item;
+            }
+
+            @Override
+            public void remove() {
+                throw new UnsupportedOperationException();
             }
         };
     }
